96202c7f1955df233d3109a83c1033337bf6cebd
Alexis Brissard <alexis.brissard@epitech.eu>
2014-05-02 00:56:09 +0900

    Add player physics

---
+++ Assets/MidTerm/Script/Game/PlayerPhysic.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerPhysic : MonoBehaviour {
+	
+	public	float	pushPower;
+	
+	void OnControllerColliderHit(ControllerColliderHit hit) {
+		Rigidbody body = hit.collider.attachedRigidbody;
+		if (hit.gameObject.rigidbody == null)
+			return;
+		if (body == null || body.isKinematic)
+			return;
+		
+		if (hit.moveDirection.y < -0.3F)
+			return;
+		Debug.Log(42);
+		Vector3 pushDir = new Vector3(hit.moveDirection.x, 0, hit.moveDirection.z);
+		body.AddForce(pushDir * pushPower);
+	}
+}
