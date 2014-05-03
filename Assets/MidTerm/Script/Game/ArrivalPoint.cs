6c2008c733fcf24a09a743dfbda8b97c8e021086
Alexis Brissard <alexis.brissard@epitech.eu>
2014-05-04 04:39:22 +0900

    Add time arrival

--- Assets/MidTerm/Script/Game/ArrivalPoint.cs
+++ Assets/MidTerm/Script/Game/ArrivalPoint.cs
@@ -3,9 +3,17 @@ using System.Collections;
 
 public class ArrivalPoint : MonoBehaviour {
 
+	private float enterTime = 0;
+
 	//private Unity callbacks
 	private	void	OnTriggerEnter(Collider other) {
 		if (other.tag == "Player") {
+			enterTime = Time.time;
+		}
+	}
+
+	private void Update() {
+		if (enterTime != 0 && Time.time - enterTime > 0.3) {
 			GameLevel.Instance.EndGame();
 		}
 	}
