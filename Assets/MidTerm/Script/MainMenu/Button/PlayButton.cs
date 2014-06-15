41ea21bb24a27ae92647363e0ab668c5aa013760
Nicolas Ricard <velvounet@gmail.com>
2014-06-15 18:27:51 +0900

    Select Niveau system.

--- Assets/MidTerm/Script/MainMenu/Button/PlayButton.cs
+++ Assets/MidTerm/Script/MainMenu/Button/PlayButton.cs
@@ -4,7 +4,22 @@ using System.Collections;
 public class PlayButton : Selectable {
 	
 	override public void Run() {
-		Application.LoadLevel("ChallengingLevel");
+		Debug.Log("LoadLevel");
+		GameObject levels = GameObject.Find("levels");
+		if (!levels) {
+			return ;
+		}
+		Debug.Log("LoadLevel");
+		SelectNiveau selector = levels.GetComponent<SelectNiveau>();
+		if (!selector) {
+			return ;
+		}
+		Debug.Log("LoadLevel");
+		string level_name = selector.getLevelName();
+		if (level_name == "") {
+			return ;
+		}
+		Debug.Log("LoadLevel");
+		Application.LoadLevel(level_name);
 	}
-
 }
---
+++ Assets/MidTerm/Script/MainMenu/SelectNiveau.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class SelectNiveau : MonoBehaviour {
+
+	public TextMesh leftArrow;
+	public TextMesh rightArrow;
+	public List<GameObject> levels = new List<GameObject>();
+	public List<string> level_names = new List<string>();
+	public int currentLevel = 0;
+	// Use this for initialization
+	void Start () {
+		UpdateCurrent();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		int tmpCurrent = currentLevel;
+		if (canLeft() && Input.GetButtonDown("MenuLeft")) {
+			currentLevel--;
+		}  
+		else if (canRight() && Input.GetButtonDown("MenuRight")) {
+			currentLevel++;
+		}
+		if (tmpCurrent != currentLevel) {
+			UpdateCurrent();
+		}
+	}
+	void UpdateCurrent() {
+		foreach (GameObject l_object in levels) {
+			l_object.SetActive(false);
+		}
+		levels[currentLevel].SetActive(true);
+		if (leftArrow != null) {
+			if (canLeft()) {
+				leftArrow.color = Color.black;
+			}
+			else {
+				leftArrow.color = Color.gray;
+			}
+		}
+		if (rightArrow != null) {
+			if (canRight()) {
+				rightArrow.color = Color.black;
+			}
+			else {
+				rightArrow.color = Color.gray;
+			}
+		}
+	}
+	public string getLevelName() {
+		if (currentLevel < level_names.Count) {
+			return level_names[currentLevel];
+		}
+		return "";
+	}
+	bool canLeft() {
+		return currentLevel != 0;
+	}
+	bool canRight() {
+		return currentLevel + 1 != levels.Count;
+	}
+}
