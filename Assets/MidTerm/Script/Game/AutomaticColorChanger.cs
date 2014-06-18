e00ee887c732c3ba35aa86fa87587d9de10387ba
Alexis Brissard <alexis.brissard@epitech.eu>
2014-06-18 14:17:17 +0900

    Had start delay automatic color change

--- Assets/MidTerm/Script/Game/AutomaticColorChanger.cs
+++ Assets/MidTerm/Script/Game/AutomaticColorChanger.cs
@@ -7,9 +7,14 @@ public class AutomaticColorChanger : MonoBehaviour {
 
 	public	GameLevel.GameColor[]	Colors;
 	public	float			TransitionDelay = 1.0f;
+	public	float			StartDelay = 0;
 
 	private	float			LastTransitionTime = 0.0f;
 	private	int				CurrentColorIndex = 0;
+
+	private void	Start() {
+		LastTransitionTime += StartDelay;
+	}
 
 	private void	Update() {
 
