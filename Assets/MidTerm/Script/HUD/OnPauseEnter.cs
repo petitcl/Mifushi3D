95c3b466efecd298e1a80e8cc7fd946b0543f647
NicolasRicard <nicolas.ricard@epitech.eu>
2014-06-18 14:18:06 -0800

    When pause is activated, now, enter or return load the menu scene.

---
+++ Assets/MidTerm/Script/HUD/OnPauseEnter.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OnPauseEnter : MonoBehaviour {
+
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.KeypadEnter) || Input.GetKeyDown(KeyCode.Return)) {
+			Application.LoadLevel("MainMenu");
+		}
+		                   
+	}
+}
