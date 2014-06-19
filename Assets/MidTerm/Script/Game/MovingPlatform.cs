ccb452b4d7c52151aacadc00c56223b9c4cad95d
freedonaab <freedonaab@gmail.com>
2014-06-18 23:13:04 -0700

    fixed moving platforms

--- Assets/MidTerm/Script/Game/MovingPlatform.cs
+++ Assets/MidTerm/Script/Game/MovingPlatform.cs
@@ -7,28 +7,54 @@ public class MovingPlatform : MonoBehaviour {
 	public	Transform	endPosition;
 	public	bool		flip;
 	public	float		speed;
+	public	bool		isTime = false;
 	public	float		delayFlip = 0;
 	public	float		delayUnFlip = 0;
 	public	bool		enabledAtStart = true;
 
 	private	float		timeLastFlip;
+	private	float		timeLastFlipEnded;
 
 	private void Start() {
-		timeLastFlip = Time.fixedTime;
+		timeLastFlip = Time.time;
+		timeLastFlipEnded = Time.time;
 		if (!this.enabledAtStart) this.enabled = false;
 	}
 
 	private void	FixedUpdate() {
-		float step = this.speed * Time.deltaTime;
+		if (!this.isTime) {
+			float step = this.speed * Time.deltaTime;
+			
+			Transform destination = this.flip ? this.endPosition : this.startPosition;
+			
+			//Stop moving during flip
+			if (Time.time - timeLastFlip > GetDelay()) {
+				this.transform.position = Vector3.MoveTowards(this.transform.position, destination.position, step);
+				if (Vector3.SqrMagnitude(this.transform.position - destination.position) < 0.01f) {
+					timeLastFlip = Time.time;
+					this.flip = !this.flip;
+					timeLastFlipEnded = Time.time + GetDelay();
+				}
+			}
+		}
+		else {
+//			float arrivalTime = this.timeLastFlipEnded + this.speed;
+			float currentOffset = (Time.time - this.timeLastFlipEnded) / (this.speed);
+			currentOffset = Mathf.Clamp01(currentOffset);
+//			Debug.Log(this.timeLastFlipEnded);
+//			Debug.Log(arrivalTime);
+//			Debug.Log(currentOffset);
 
-		Transform destination = this.flip ? this.endPosition : this.startPosition;
+			Transform start = !this.flip ? this.endPosition : this.startPosition;
+			Transform destination = this.flip ? this.endPosition : this.startPosition;
 
-		//Stop moving during flip
-		if (Time.fixedTime - timeLastFlip > GetDelay()) {
-			this.transform.position = Vector3.MoveTowards(this.transform.position, destination.position, step);
-			if (Vector3.SqrMagnitude(this.transform.position - destination.position) < 0.01f) {
-				this.flip = !this.flip;
-				timeLastFlip = Time.fixedTime;
+			if (Time.time > timeLastFlipEnded) {
+				this.transform.position = Vector3.Lerp(start.position, destination.position, currentOffset);
+				if (Vector3.SqrMagnitude(this.transform.position - destination.position) < 0.01f) {
+					timeLastFlip = Time.time;
+					this.flip = !this.flip;
+					timeLastFlipEnded = Time.time + GetDelay();
+				}
 			}
 		}
 	}
